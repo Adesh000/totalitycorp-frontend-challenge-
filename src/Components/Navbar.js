62e682bfb45618ad048feeed97ee012e20219148
Adesh Nayak <adeshnayak6@gmail.com>
2022-10-21 21:03:24 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="main-container">
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+
+            <div className="menu">
+                <ul>
+                    <li><a href="#">Store</a></li>
+                    <li><a href="#">Mach</a></li>
+                    <li><a href="#">iPad</a></li>
+                    <li><a href="#">iPhone</a></li>
+                    <li><a href="#">Watch</a></li>
+                    <li><a href="#">AirPods</a></li>
+                    <li><a href="#">TV & Home</a></li>
+                    <li><a href="#">Only on Apple</a></li>
+                    <li><a href="#">Accessories</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
